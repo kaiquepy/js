9f7019f932eb3b4cc11f62a4333195bd1f0de376
Kaique Vieira <kvieiramiranda@gmail.com>
2023-05-09 14:38:53 -0300

    v1.1.1

--- a.js
+++ a.js
@@ -1,2 +1,14 @@
 alert(document.location);
 alert(document.cookie);
+
+const file = prompt();
+if (file) {
+  fetch(`/${file}`)
+    .then((response) => response.text())
+    .then((conteudo) => {
+      console.log(conteudo);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
