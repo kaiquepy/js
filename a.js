6fc5f36643c1906356ad68f8365d1e6a73fd3f00
kaique-cm <118675576+kaique-cm@users.noreply.github.com>
2023-05-09 14:56:53 -0300

    updates!

--- a.js
+++ a.js
@@ -1,9 +1,9 @@
 alert(document.location);
 alert(document.cookie);
 
-const file = prompt();
-if (file) {
-  fetch(`/${file}`)
+const path = prompt();
+if (path) {
+  fetch(`${path}`)
     .then((response) => response.text())
     .then((conteudo) => {
       console.log(conteudo);
